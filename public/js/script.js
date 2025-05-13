d11b3d2e821f34b37c1af5d54bb8ab3012937a8d
gotayjosue <gotayjosue@gmail.com>
2025-05-13 01:53:16 -0600

    home page finished

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-const background = document.getElementById('background')
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth >= 768) {
-        background.classList.add('visible');
-    }else{
-        background.classList.add('hide')
-    }
-})
