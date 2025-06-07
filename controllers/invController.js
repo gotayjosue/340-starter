67799fcde2b62d5fc3837bc565af633d9b2c64c3
gotayjosue <gotayjosue@gmail.com>
2025-06-07 13:08:12 -0600

    Team activity finished

--- controllers/invController.js
+++ controllers/invController.js
@@ -294,5 +294,91 @@ invCont.updateInventory = async function (req, res) {
   }
 }
 
+/* ***************************
+ *  Build delete vehicle view
+ * ************************** */
+
+invCont.deleteVehicleView = async function (req, res, next){
+  const inv_id = parseInt(req.params.inv_id)
+  let nav = await utilities.getNav()
+  console.log(inv_id)
+  const itemData = await invModel.getVehicleByInventoryId(inv_id)
+  const classificationSelect = await utilities.buildClassificationList(itemData.classification_id)
+  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
+
+  res.render('inventory/delete-confirm', {
+    title: "Delete " + itemName,
+    nav,
+    classificationSelect: classificationSelect,
+    errors: null,
+    inv_id: itemData.inv_id,
+    inv_make: itemData.inv_make,
+    inv_model: itemData.inv_model,
+    inv_year: itemData.inv_year,
+    inv_price: itemData.inv_price,
+    classification_id: itemData.classification_id
+  })
+}
+
+/* ***************************
+ *  Delete a car function
+ * ************************** */
+invCont.deleteInventory = async function (req, res) {
+  let nav = await utilities.getNav()
+  const {
+    inv_id,
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    classification_id,
+  } = req.body
+  const updateResult = await invModel.deleteInventory(
+    inv_id,  
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    classification_id
+  )
+
+  if (updateResult) {
+    const itemName = updateResult.inv_make + " " + updateResult.inv_model
+    req.flash("notice", "The deletion was successful")
+    res.redirect("/inv/")
+  } else {
+    const classificationSelect = await utilities.buildClassificationList(classification_id)
+    const itemName = `${inv_make} ${inv_model}`
+    req.flash("notice", "Sorry, the delete failed.")
+    res.status(501).render("inventory/delete-confirm", {
+    title: "Delete " + itemName,
+    nav,
+    classificationSelect: classificationSelect,
+    errors: null,
+    inv_id,
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id
+    })
+  }
+}
+
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,27 +4,31 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 const classValidate = require("../utilities/addClass-validation")
 const invValidate = require("../utilities/addVehicle-validation")
+const utilities = require("../utilities")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 //Route to build the detail view
-router.get("/detail/:inventoryId", invController.buildDetailView)
+router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildDetailView))
 
 //Route to build the classification view
-router.get('/', invController.managementView)
+router.get('/', utilities.handleErrors(invController.managementView))
 
 //Route to build the "Add Classification" view
-router.get('/add-classification', invController.addClassificationView)
+router.get('/add-classification', utilities.handleErrors(invController.addClassificationView))
 
 //Route to build the "Add New Vehicle" view
-router.get('/add-vehicle', invController.addVehicleView)
+router.get('/add-vehicle', utilities.handleErrors(invController.addVehicleView))
 
 //Route to build the url inside the javascript file
-router.get('/getInventory/:classification_id', invController.getInventoryJSON)
+router.get('/getInventory/:classification_id', utilities.handleErrors(invController.getInventoryJSON))
 
 //Route to build the update car view
-router.get('/edit/:inv_id', invController.editVehicleView)
+router.get('/edit/:inv_id', utilities.handleErrors(invController.editVehicleView))
+
+//Route to build the delete car view
+router.get('/delete/:inv_id', utilities.handleErrors(invController.deleteVehicleView))
 
 
 
@@ -50,5 +54,8 @@ router.post(
     invValidate.inventoryRules(),
     invValidate.checkUpdateData,
     invController.updateInventory)
+
+//Route to delete a car from the inventory
+router.post("/delete-confirm", invController.deleteInventory)
 
 module.exports = router;
