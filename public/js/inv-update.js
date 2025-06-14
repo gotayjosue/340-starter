678f53f8d645ca7b77f5091fc29389fab01b419c
gotayjosue <gotayjosue@gmail.com>
2025-06-14 00:45:41 -0600

    Form error fixed

--- public/js/inv-update.js
+++ public/js/inv-update.js
@@ -1,11 +1,20 @@
-const form = document.querySelector('.editCar')
-    form.addEventListener('change', function(){
-        const updateBtn = document.querySelector('button')
-        updateBtn.removeAttribute('disabled')
-    })
 
-const form2 = document.querySelector('.updateClassificationData')
-    form2.addEventListener('change', function(){
-        const updateBtn2 = document.querySelector('button')
-        updateBtn2.removeAttribute('disabled')
-    })
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.editCar')
+    if (form) {
+        form.addEventListener('change', function(){
+            const updateBtn = document.querySelector('button')
+            if (updateBtn) updateBtn.removeAttribute('disabled')
+        })
+    }
+
+
+    const form2 = document.querySelector('#updateClass')
+    if (form2){
+    
+        form2.addEventListener('change', function(){
+            const updateBtn2 = document.querySelector('#updateClassButton')
+            if (updateBtn2) updateBtn2.removeAttribute('disabled')
+        })
+    }
+})
